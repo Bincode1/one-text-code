bb9fa7b2a4719c2a56d2933ce12b2ddb3b407ec7
coco <xiabincode@163.com>
2025-08-12 12:26:09 +0800

    - 后端用户模块开发 1. 开发MyBatis-Flex代码生成器 2. 开发核心功能，登陆、注册、注销、获取登陆用户信息等 3. 自定义权限校验注解，进行全局权限校验 4. 开发管理员增删改查接口 5. 配置jackson序列化，防止精度丢失

--- src/main/java/com/bin/onetextcode/OneTextCodeApplication.java
+++ src/main/java/com/bin/onetextcode/OneTextCodeApplication.java
@@ -1,9 +1,11 @@
 package com.bin.onetextcode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bin.onetextcode.mapper")
 public class OneTextCodeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bin/onetextcode/exception/ErrorCode.java
+++ src/main/java/com/bin/onetextcode/exception/ErrorCode.java
@@ -8,9 +8,9 @@ import java.util.Map;
 public enum ErrorCode {
 
     SUCESS(0,"OK"),
-    PARAM_ERROR(10000,"参数错误"),
+    PARAMS_ERROR(10000,"参数错误"),
     NOT_LOGIN_ERROR(40001,"未登录"),
-    NOT_AUTH_ERROR(40002,"无权限"),
+    NO_AUTH_ERROR(40002,"无权限"),
     NOT_FOUND_ERROR(40400,"请求数据访问不存在"),
     FORBIDDEN_ERROR(40300,"禁止访问"),
     SYSTEM_ERROR(50000,"系统错误"),
